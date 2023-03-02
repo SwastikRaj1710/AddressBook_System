012b87248e43ee33235b60e878b3fa28d1a82b3a
SwastikRaj1710 <swastikraj1710@gmail.com>
2023-03-02 13:30:57 +0530

    [Swastik] Added an AddressBook class which contains an AddContact function that takes in the user details and creates a new contact in the Address Book

--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -7,6 +7,29 @@ namespace Address_Book_System
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Add a new Contact to the Address Book");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine("Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Adding a new contact to the address book");
+                        AddressBook addressBook = new AddressBook();
+                        addressBook.AddContact();
+                        break;
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect Choice!");
+                        break;
+                }
+            }
         }
     }
 }
